6dfbab7faaeeb844e53605ef833786fcfcf7579e
chirag-bgh <chirag2bsingh@gmail.com>
2022-10-04 23:35:31 +0530

    wip

--- test/0xbadcode.t.sol
+++ test/0xbadcode.t.sol
@@ -1,13 +1,34 @@
 // SPDX-License-Identifier: MIT
 
 /// @notice 
-/// @author chirag-bgh
+/// @author https://github.com/chirag-bgh
 
 pragma solidity 0.8.17;
 
 import "forge-std/Test.sol";
+import "./interfaces/IERC20.sol";
+
+// interface DyDxPool {
+//     function getAccountWei(Info memory account, uint256 marketId) external view returns (Wei memory);
+//     function operate(Info[] memory, ActionArgs[] memory) external;
+// }
 
 contract Exploit is Test {
+
+    IERC20 weth = IERC20(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
+    uint256 mainnetFork;
+    address exploiter;
+
+    function setUp() public {
+
+        // create fork at block 15625424
+        // mainnetFork = vm.createFork(MAINNET_RPC_URL, 15625424);
+        exploiter = vm.addr(31337);
+        console.log(exploiter);
+
+
+    }
+
 
     
 }
